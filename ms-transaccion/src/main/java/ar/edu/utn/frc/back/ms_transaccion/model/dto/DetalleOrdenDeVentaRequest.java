f77a257e40a3310defaae1f8f68b4facb92fc6d5
FrancoUTN2025 <81755@sistemas.frc.utn.edu.ar>
2026-06-23 23:20:42 -0300

    arreglado problema en el DTO de ordenes de venta

--- ms-transaccion/src/main/java/ar/edu/utn/frc/back/ms_transaccion/model/dto/DetalleOrdenDeVentaRequest.java
+++ ms-transaccion/src/main/java/ar/edu/utn/frc/back/ms_transaccion/model/dto/DetalleOrdenDeVentaRequest.java
@@ -1,10 +1,14 @@
 package ar.edu.utn.frc.back.ms_transaccion.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@Getter
 public class DetalleOrdenDeVentaRequest {
     private String simboloAccion;
     private Long cantidad;
--- ms-transaccion/src/main/java/ar/edu/utn/frc/back/ms_transaccion/model/dto/OrdenDeVentaRequest.java
+++ ms-transaccion/src/main/java/ar/edu/utn/frc/back/ms_transaccion/model/dto/OrdenDeVentaRequest.java
@@ -1,12 +1,16 @@
 package ar.edu.utn.frc.back.ms_transaccion.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@Getter
 public class OrdenDeVentaRequest {
     private Long usuarioId;
     private List<DetalleOrdenDeVentaRequest> detalles;
